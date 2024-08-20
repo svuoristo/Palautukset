aa66c9abf7e18bf1d761330dbe2842616b217240
senja <senja.vuoristo@aalto.fi>
2024-08-20 16:29:03 +0300

    Poistettiin turha print-komento tehtävästä 5.

--- Module2/05-yksiköt.py
+++ Module2/05-yksiköt.py
@@ -2,11 +2,15 @@ leiviskat = float(input("Anna leiviskät.\n"))
 naulat = float(input("Anna naulat.\n"))
 luodit = float(input("Anna luodit.\n"))
 
-print("Massa nykymittojen mukaan:")
-
-grammoina = luodit * 13.3 + naulat * 32 * 13.3 + leiviskat * 20 * 32 * 13.3
+grammoina = (luodit * 13.3
+             + naulat * 32 * 13.3
+             + leiviskat * 20 * 32 * 13.3
+             )
 
 kilot = int(grammoina/1000)
 grammat = grammoina - kilot * 1000
 
-print("Massa nykymittojen mukaan:\n" + str(kilot) + f" kilogrammaa ja {grammat:.2f} grammaa.")
+print("Massa nykymittojen mukaan:\n"
+      + str(kilot)
+      + f" kilogrammaa ja {grammat:.2f} grammaa."
+      )
