d7be523fb2e21225805639837b480aea772bfc27
senja <senja.vuoristo@aalto.fi>
2024-09-15 16:33:53 +0300

    Moduuli 8 t. 2

---
+++ Module8/02kenttienlkm.py
@@ -0,0 +1,24 @@
+import mysql.connector
+
+connection = mysql.connector.connect(
+         host='localhost',
+         port= 3306,
+         database='flight_game',
+         user='palautukset',
+         password='moro3',
+         autocommit=True,
+         collation = "utf8mb4_general_ci",
+         )
+
+isocountry = input("Anna maakoodi: ")
+
+amounts = f"SELECT type, COUNT(*) FROM Airport WHERE iso_country = '{isocountry}' GROUP BY type"
+print(amounts)
+
+cursor = connection.cursor()
+cursor.execute(amounts)
+result = cursor.fetchall()
+print(result)
+
+for row in result:
+    print(f"{row[0].replace('_',' ')}: {row[1]}")
